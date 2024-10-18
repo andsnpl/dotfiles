167ed923bed40691c97144e9e22cc6b57da87bdc
Paul Anderson <paul@andersonpaul.com>
2024-10-17 23:53:15 -0500

    Update .aerospace.toml Update .config/nvim/lua/paul/core/keymaps.lua

--- dot_config/nvim/lua/paul/core/keymaps.lua
+++ dot_config/nvim/lua/paul/core/keymaps.lua
@@ -8,6 +8,18 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sc", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set(
+	"n",
+	"<C-s>",
+	'viw"ry:%s/<C-r>r/<C-r>r/gc<Left><Left><Left>',
+	{ desc = "Start substitution on the word under the cursor" }
+)
+keymap.set(
+	"x",
+	"<C-s>",
+	'"ry<esc>:%s/<C-r>r/<C-r>r/gc<Left><Left><Left>',
+	{ desc = "Start substitution on the text under the cursor" }
+)
 keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write the current buffer" })
 keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit Neovim" })
 keymap.set("n", "<leader>R", function()
