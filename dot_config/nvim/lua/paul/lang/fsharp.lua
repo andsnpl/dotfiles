5fe1dd6b9031f82058f00dcfe89ce3452a88bbf5
Paul Anderson <paul@andersonpaul.com>
2025-01-05 05:13:34 -0600

    Add .config/nvim/after/ftplugin/fsharp.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/paul/lang/fsharp.lua Add .config/nvim/lua/paul/lang/ocaml.lua

---
+++ dot_config/nvim/lua/paul/lang/fsharp.lua
@@ -0,0 +1,11 @@
+return {
+	treesitter_parsers = {
+		"fsharp",
+	},
+	language_servers = {
+		"fsautocomplete",
+	},
+	formatters_by_ft = {
+		fsharp = { "fantomas" },
+	},
+}
---
+++ dot_config/nvim/lua/paul/lang/ocaml.lua
@@ -0,0 +1,11 @@
+return {
+	treesitter_parsers = {
+		"ocaml",
+	},
+	language_servers = {
+		"ocaml-lsp",
+	},
+	formatters_by_ft = {
+		ocaml = { "ocamlformat" },
+	},
+}
